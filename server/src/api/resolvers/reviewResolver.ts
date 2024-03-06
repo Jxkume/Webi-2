fd5e46866011f2a45af0ba2ccaca924b0fea7823
Jhon <jhon.RastrojoLopez@metropolia.fi>
2024-03-06 23:13:08 +0200

    all review pages done and functioning + comment things also done

--- server/src/api/resolvers/reviewResolver.ts
+++ server/src/api/resolvers/reviewResolver.ts
@@ -2,29 +2,39 @@ import { Review } from "../../types/DBtypes";
 import reviewModel from "../models/reviewModel";
 import MyContext from "../../types/MyContext";
 import { isLoggedIn } from "../../functions/authorize";
+import commentModel from "../models/commentModel";
 
 export default {
   Query: {
     reviews: async () => {
       return await reviewModel.find();
     },
-    review: async (_parent: undefined, args: {id: string}) => {
+    review: async (_parent: undefined, args: { id: string }) => {
       return await reviewModel.findById(args.id);
     },
-    reviewsByCategory: async (_parent: undefined, args: {categoryId: string}) => {
-      return await reviewModel.find({category: args.categoryId});
+    reviewsByCategory: async (
+      _parent: undefined,
+      args: { categoryId: string }
+    ) => {
+      return await reviewModel.find({ category: args.categoryId });
     },
-    reviewsByAuthor: async (_parent: undefined, args: {authorId: string}) => {
-      return await reviewModel.find({author: args.authorId});
+    reviewsByAuthor: async (_parent: undefined, args: { authorId: string }) => {
+      return await reviewModel.find({ author: args.authorId });
     },
-    reviewsByRating: async (_parent: undefined, args: {rating: number}) => {
-      return await reviewModel.find({rating: args.rating});
+    reviewsByRating: async (_parent: undefined, args: { rating: number }) => {
+      return await reviewModel.find({ rating: args.rating });
     },
   },
+  Review: {
+    comments: async (parent: any) => {
+      return await commentModel.find({ post: parent.id });
+    },
+  },
+
   Mutation: {
     addReview: async (
       _parent: undefined,
-      args: {input: Omit<Review, 'id'>},
+      args: { input: Omit<Review, "id"> },
       context: MyContext
     ) => {
       isLoggedIn(context);
@@ -34,29 +44,33 @@ export default {
     },
     updateReview: async (
       _parent: undefined,
-      args: {id: string, input: Partial<Omit<Review, 'id'>>},
+      args: { id: string; input: Partial<Omit<Review, "id">> },
       context: MyContext
     ) => {
       isLoggedIn(context);
-      if(context.userdata?.user.role !== 'admin') {
-        const filter = {_id: args.id, author: context.userdata?.user.id};
-        return await reviewModel.findOneAndUpdate(filter, args.input, {new: true});
+      if (context.userdata?.user.role !== "admin") {
+        const filter = { _id: args.id, author: context.userdata?.user.id };
+        return await reviewModel.findOneAndUpdate(filter, args.input, {
+          new: true,
+        });
       } else {
-        return await reviewModel.findByIdAndUpdate(args.id, args.input, {new: true});
+        return await reviewModel.findByIdAndUpdate(args.id, args.input, {
+          new: true,
+        });
       }
     },
     deleteReview: async (
       _parent: undefined,
-      args: {id: string},
+      args: { id: string },
       context: MyContext
     ) => {
       isLoggedIn(context);
-      if(context.userdata?.user.role !== 'admin') {
-        const filter = {_id: args.id, author: context.userdata?.user.id};
+      if (context.userdata?.user.role !== "admin") {
+        const filter = { _id: args.id, author: context.userdata?.user.id };
         return await reviewModel.findOneAndDelete(filter);
       } else {
         return await reviewModel.findByIdAndDelete(args.id);
       }
-    }
-  }
-}
+    },
+  },
+};
