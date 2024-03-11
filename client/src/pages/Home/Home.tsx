c4bfd941fa38309c86b6f800880e8c43147b58ea
Korpi <104062080+suokiiski@users.noreply.github.com>
2024-03-11 17:43:37 +0200

    homepage napit kirjautuneelle

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,16 +3,21 @@ import { useNavigate } from 'react-router-dom';
 import { Container, Button, Form, Row, Col } from "react-bootstrap";
 import './Home.css';
 import HeroTitleImage from './hero-title.png';
+import { getCookie} from "typescript-cookie";
 
 const Home = () => {
   const [searchInput, setSearchInput] = useState('');
   const navigate = useNavigate();
+  const isLoggedin = !!getCookie('token');
 
-  const handleRegisterClick = () => {
-    navigate('/rekisteroidy');
+  const handleButtonClick = () => {
+    if (isLoggedin) {
+      navigate('/uusiArvostelu');
+    } else  {
+      navigate('/rekisteroidy');
+    }
   }
 
-  
   const handleSearchInputChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
     setSearchInput(event.target.value);
   }
@@ -20,6 +25,7 @@ const Home = () => {
   const handleSearchClick = () => {
     navigate(`/haku/${searchInput}`);
   }
+
   return (
     <div className="hero-section">
       <div className="overlay">
@@ -29,21 +35,21 @@ const Home = () => {
               <img src={HeroTitleImage} alt="Delightful Insights" className="logo-image mb-3" />
             </Col>
           </Row>
-          
+
           <Row className="justify-content-center mb-4">
             <Col xs={12} md={8} lg={6}>
               <Form className="d-flex justify-content-center">
                 <div className="d-flex">
                   <Form.Control type="text" placeholder="Hae tuotetta..." className="me-2 search-input" value={searchInput} onChange={handleSearchInputChange} />
-                  <Button variant="primary" className="search-button" onClick={handleSearchClick}>Hae</Button>
+                  <Button variant="primary" className="btn-custom" onClick={handleSearchClick}>Hae</Button>
                 </div>
               </Form>
             </Col>
           </Row>
-          
+
           <Row className="justify-content-center">
             <Col xs={12} md={8} lg={6} className="text-center">
-              <Button className="register-button mt-3" onClick={handleRegisterClick}>Rekisteröidy nyt!</Button>
+              <Button className="btn-custom mt-3" onClick={handleButtonClick}>{isLoggedin ? 'Kirjoita arvostelu!' : 'Rekisteröidy nyt!'}</Button>
             </Col>
           </Row>
         </Container>
